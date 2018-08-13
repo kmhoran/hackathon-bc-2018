a0203cc98075e8718f596c81a0ad076e80fd1206
millie.savalia <millie.savalia@beautycounter.com>
2018-08-13 16:29:15 -0700

    upcoming

--- src/Components/Shared/ProductsJson.js
+++ src/Components/Shared/ProductsJson.js
@@ -15,7 +15,33 @@ const ProductsJson = {
         title: "fruit",
         img: require("../../Assets/products/fruit.jpg")   
     }
-   ]
+   ],
+
+   upcoming: [{
+        id: 1,
+        title: "Countersun Sunscreen",
+        img: require("../../Assets/products/upcoming-img/sunscreen.png")
+   },
+   {
+        id: 2,
+        title: "Sur La Table Green Pan",
+        img: require("../../Assets/products/upcoming-img/pan.png")
+   },
+   {
+        id: 3,
+        title: "GoopGlow Superpowder",
+        img: require("../../Assets/products/upcoming-img/powder.png")
+   },
+   {
+        id: 4,
+        title: "MNDFL Meditation Package",
+        img: require("../../Assets/products/upcoming-img/mat.png")
+   },
+   {
+        id: 6,
+        title: "Thistle Plant-Based Meals",
+        img: require("../../Assets/products/upcoming-img/bowl.png")
+   }]
 }
 
 export default ProductsJson;
